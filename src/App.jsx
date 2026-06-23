2fd22b34bba3e4c89557c3ba232c104f6661bae9
Dan <danakwaji3@gmail.com>
2026-06-23 03:08:18 -0700

    -

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,65 @@
-import { Routes, Route } from "react-router-dom";
+import {
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
+
+import { AnimatePresence } from "framer-motion";
 
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import ScrollToTop from "./components/ScrollToTop";
 
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Packages from "./pages/Packages";
 import Testimonials from "./pages/Testimonials";
-// import Blog from "./pages/Blog";
-import FAQ from "./pages/FAQ"
+import FAQ from "./pages/FAQ";
 import Payment from "./pages/payment";
-import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
+  const location = useLocation();
+
   return (
     <>
-
       <ScrollToTop />
-
       <Navbar />
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/packages" element={<Packages />} />
-        <Route path="/testimonials" element={<Testimonials />} />
-        {/* <Route path="/blog" element={<Blog />} /> */}
-        <Route path="/faq" element={<FAQ />} />
-        <Route path="/payment" element={<Payment />} />
-      </Routes>
+      <AnimatePresence mode="wait">
+
+        <Routes
+          location={location}
+          key={location.pathname}
+        >
+          <Route path="/" element={<Home />} />
+
+          <Route
+            path="/about"
+            element={<About />}
+          />
+
+          <Route
+            path="/packages"
+            element={<Packages />}
+          />
+
+          <Route
+            path="/testimonials"
+            element={<Testimonials />}
+          />
+
+          <Route
+            path="/faq"
+            element={<FAQ />}
+          />
+
+          <Route
+            path="/payment"
+            element={<Payment />}
+          />
+        </Routes>
+
+      </AnimatePresence>
 
       <Footer />
     </>
---
+++ src/components/PageTransition.jsx
@@ -0,0 +1,26 @@
+import { motion } from "framer-motion";
+
+export default function PageTransition({ children }) {
+  return (
+    <motion.div
+      initial={{
+        opacity: 0,
+        x: 50,
+      }}
+      animate={{
+        opacity: 1,
+        x: 0,
+      }}
+      exit={{
+        opacity: 0,
+        x: -50,
+      }}
+      transition={{
+        duration: 0.4,
+        ease: "easeInOut",
+      }}
+    >
+      {children}
+    </motion.div>
+  );
+}
